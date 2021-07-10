26a499570f1a402c699db676e07e209ddf2f3b0e
moruiyi <emwings@outlook.com>
2021-07-11 00:56:52 +0800

    feat: i am small commit

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -4,39 +4,50 @@ using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.Events;
-public class UIManager : MonoBehaviour 
+using UnityEngine.SceneManagement;
+public class UIManager : MonoBehaviour
 {
-    public Button btn1;
-   //...
- 
+    public Button startBtn;
+    public Button exitBtn;
+
     void Awake()
     {
-        btn1 = this.transform.Find("Button").GetComponent<Button>();
-        AddTriggersListener(btn1.gameObject, EventTriggerType.PointerClick, StartGame);
+        // AddTriggersListener(startBtn.gameObject, EventTriggerType.PointerClick, StartGame);
+        startBtn.onClick.AddListener(StartGame);
+        exitBtn.onClick.AddListener(ExitGame);
     }
- 
-   
-    void StartGame(BaseEventData data)
+
+    void StartGame()
     {
-        Debug.Log("start game");
+        SceneManager.LoadSceneAsync("Gaming", LoadSceneMode.Single);
     }
- 
-    private void AddTriggersListener(GameObject obj, EventTriggerType eventID, UnityAction<BaseEventData> action)
+
+    void ExitGame()
     {
-        EventTrigger trigger = obj.GetComponent<EventTrigger>();
-        if (trigger == null)
-        {
-            trigger = obj.AddComponent<EventTrigger>();
-        }
- 
-        if (trigger.triggers.Count == 0)
-        {
-            trigger.triggers = new List<EventTrigger.Entry>();
-        }
- 
-        UnityAction<BaseEventData> callback = new UnityAction<BaseEventData>(action);
-        EventTrigger.Entry entry = new EventTrigger.Entry();
-        entry.eventID = eventID;
-        entry.callback.AddListener(callback);
-        trigger.triggers.Add(entry);
-}}
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();        
+#else
+        Application.Quit();
+#endif
+    }
+
+    // private void AddTriggersListener(GameObject obj, EventTriggerType eventID, UnityAction<BaseEventData> action)
+    // {
+    //     EventTrigger trigger = obj.GetComponent<EventTrigger>();
+    //     if (trigger == null)
+    //     {
+    //         trigger = obj.AddComponent<EventTrigger>();
+    //     }
+
+    //     if (trigger.triggers.Count == 0)
+    //     {
+    //         trigger.triggers = new List<EventTrigger.Entry>();
+    //     }
+
+    //     UnityAction<BaseEventData> callback = new UnityAction<BaseEventData>(action);
+    //     EventTrigger.Entry entry = new EventTrigger.Entry();
+    //     entry.eventID = eventID;
+    //     entry.callback.AddListener(callback);
+    //     trigger.triggers.Add(entry);
+    // }
+}
