c7b4a406e2aff2dfb2a9d358eb7749918efa2041
moruiyi <emwings@outlook.com>
2021-07-10 13:28:20 +0800

    feat: 上模型

---
+++ Assets/CanFlyObstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("障碍物/可撞飞")]
+public class CanFlyObstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Collider _collider;
+    void Start()
+    {
+        _collider = GetComponent<Collider>() ?? gameObject.AddComponent<BoxCollider>();
+        _collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("OnTriggerEnter: " + other.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
