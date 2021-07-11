5c31e0f28dd0f66eb8dee301426237daf78ad7be
pengjunkai <2434937467@qq.com>
2021-07-11 10:55:34 +0800

    旧入轨方式

--- Assets/Traction.cs
+++ Assets/Traction.cs
@@ -31,8 +31,30 @@ public class Traction : MonoBehaviour
     {
         if (touchTrack && !controller.drag)
         {
-          
 
+            //old
+            /*
+             
+             */
+
+            float xrate = track.position.x - controller.train.transform.position.x;
+            if (Mathf.Abs(xrate) <= 0.1)
+            {
+                xrate = 0;
+            }
+            else
+            {
+                //xrate = xrate / Mathf.Abs(xrate);
+            }
+
+
+
+            Vector2 further = new Vector2(xrate * XrotateScale, 0.5f);
+
+            Vector3 vel = new Vector3(further.x, 0, further.y);
+            controller.train.body.velocity = (vel.normalized + controller.train.body.velocity.normalized).normalized * controller.train.speed;
+
+            /*
             Vector3 train2track = controller.train.transform.forward;
             float cos = Vector3.Dot(train2track.normalized, track.forward.normalized);
             Vector3 cross = Vector3.Cross(train2track.normalized, track.forward.normalized);
@@ -45,6 +67,7 @@ public class Traction : MonoBehaviour
             sin = sin <= -360 ? (sin + 360) : sin;
 
             controller.train.body.velocity = Quaternion.Euler(0f, sin * 10, 0f) * controller.train.body.velocity;
+            */
         }
     }
 
