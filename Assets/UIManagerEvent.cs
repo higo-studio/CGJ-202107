8fda7a006295c4b22296e010aa0b5c38459ab40a
Esabelle0829 <Zhouxiaoqi.kiki@bytedance.com>
2021-07-11 13:25:33 +0800

    feat:结束页

--- Assets/UIManagerEvent.cs
+++ Assets/UIManagerEvent.cs
@@ -8,11 +8,23 @@ using UnityEngine.SceneManagement;
 public class UIManagerEvent : MonoBehaviour
 {
   public GameObject gameOver;
+  public TMPro.TMP_Text gameResult;
   public CubeController controller;
 
   void GameOver(ResultType type)
   {
       Debug.Log($"GameOver, {type}");
+      switch(type) {
+        case ResultType.CannotBreath:
+          gameResult.text = "You were blown away by a sandstorm";
+          break;
+        case ResultType.Drop:
+          gameResult.text = "You fell off a cliff and died";
+          break;
+        case ResultType.Succeed:
+          gameResult.text = "Successfully reach the small cave";
+          break;
+      }
       gameOver.SetActive(true);
       Time.timeScale = 0.0f;
       controller.enabled = false;
