ccb7425b2b96b9f777365281bb7da0a5d7ce92c8
Esabelle0829 <Zhouxiaoqi.kiki@bytedance.com>
2021-07-11 12:13:57 +0800

    feat: ui脚本

---
+++ Assets/UIManagerEvent.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+public class UIManagerEvent : MonoBehaviour
+{
+  public GameObject gameOver;
+  void GameOver()
+  {
+    
+  }
+}
